6de011250821b1e5a791139e719b0d026a11f4ce
dcaputa <diego.caputa@hotmail.com>
2023-01-29 19:33:03 +0100

    homework M04

--- M03_JavaScript_Fundamentos/Ejercicio 04.js
+++ M03_JavaScript_Fundamentos/Ejercicio 04.js
@@ -6,31 +6,50 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+
+   let cuadrado = Math.pow(num, 2);
+   return (cuadrado)
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+
+  let cubo = Math.pow(num, 3);
+  return (cubo);
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   
+   let elevar = Math.pov(num, exponent);
+   return (elevar);
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+
+   let redondearNumero = Math.round(num);
+   return (redondearNumero);
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+
+   let redondearHaciaArriba = Math.ceil(num);
+   return (redondearHaciaArriba);
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+
+   let numeroRandom = Math.random(0.5);
+   return (numeroRandom);
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M03_JavaScript_Fundamentos/Ejercicio_05.js
+++ M03_JavaScript_Fundamentos/Ejercicio_05.js
@@ -8,54 +8,119 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve False.
    // Tu código:
-}
+   let dato 
+   if (num > 0) {
+      dato = "es positivo"   
+   } else {
+      dato = "es negativo"
+   }
+   console.log (dato)
+   return dato
 
+}
+esPositivo (3)
+esPositivo (0)
 function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   let dato
+   // dato = str + "!"
+   dato = `${str}!`
+   console.log (dato)
+   return dato 
 }
+
+agregarSimboloExclamacion ("hello world")
+
 
 function combinarNombres(nombre, apellido) {
    // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
    // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
+   let dato
+   dato = `${nombre} ${apellido}`
+   return dato
 }
+
+console.log (combinarNombres("Diego","Caro"))
+
 
 function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
+   let dato
+   dato = `Hola ${nombre}!`
+   return dato
 }
+
+console.log (obtenerSaludo("Martin"))
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
+   let dato
+   dato = alto * ancho
+   return dato
 }
+
+console.log (obtenerAreaRectangulo (3 , 7))
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   let dato
+   dato = lado * 4 
+   return dato
 }
+
+console.log (retornarPerimetro(8))
+
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+   let dato
+   dato = (base * altura) / 2
+   return dato 
 }
+
+console.log (areaDelTriangulo(20 , 4))
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
+   let dolar
+   dolar = euro * 1.20
+   return dolar
 }
+
+console.log (deEuroAdolar(3))
 
 function esVocal(letra) {
    // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   let dato
+   if (letra.length > 1){
+      dato = "Dato incorrecto"
+   } else {
+      if (letra == ("a"||"e"||"i"||"o"||"u")){
+         dato = "Es vocal"
+      } else {
+         dato = "Dato incorrecto"
+      }
+   }
+   return dato
 }
+
+console.log (esVocal("a"))
+console.log (esVocal("b"))
+console.log (esVocal("abc"))
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
