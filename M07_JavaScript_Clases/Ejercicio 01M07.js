bc4528aeed0ff672ee611b6621a34a2e4e3eecac
dcaputa <diego.caputa@hotmail.com>
2023-01-31 20:14:38 +0100

    M07_JavaScript_Clases

--- M07_JavaScript_Clases/Ejercicio 01M07.js
+++ M07_JavaScript_Clases/Ejercicio 01M07.js
@@ -10,18 +10,55 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   // ES6
+   // class Usuario {}
+   // funcion constructora
+   function Usuario(usuario, nombre, email, password) {
+      this.usuario = usuario;
+      this.nombre = nombre;
+      this.email = email;
+      this.password = password;
+      this.saludar = function () {
+         return "Hola, mi nombre es " + this.nombre 
+      };
+
+   }
+   return Usuario;
 }
+
+//   return `hola, mi nombre es ${this.nombre}`;
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+
+   Usuario.prototype.saludar = function () {
+      return "Hello World!";
+   }
+
+
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   // Tu código:
+   String.prototype.reverse = function (){
+      this
+      return this.split("").reverse().join("");
+   };
+   // Pasos explicación:
+   // 'hola' -> 'hola'.reverse() -> 'aloh'
+   // // 'hola'
+   // var array = this.split("");
+   // // ['h','o','l','a']
+   // var ivertido = array.reverse();
+   // // ['a','l','o','h']
+   // var final = invertido.join("");
+   // // 'aloh'
+   // return final;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
